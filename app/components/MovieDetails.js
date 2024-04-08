4605b1266a80c1f4f411f7e7b08945b6646ec0c6
masud040 <masudinfo040@.com>
2024-04-08 11:47:35 +0600

    add two version data like english and bengali for internationalization

--- app/components/MovieDetails.js
+++ app/components/MovieDetails.js
@@ -22,7 +22,7 @@ const MovieDetails = ({ movieData }) => {
       <div>
         <Image
           class="w-full object-cover max-h-[300px] lg:max-h-[500px]"
-          src="https://image.tmdb.org/t/p/original/qrGtVFxaD8c7et0jUtaYhyTzzPg.jpg"
+          src={poster_path}
           width={700}
           height={700}
           alt=""
@@ -31,22 +31,11 @@ const MovieDetails = ({ movieData }) => {
 
       <div class="grid grid-cols-12 py-12 gap-8">
         <div class="col-span-2">
-          <Image
-            src="https://image.tmdb.org/t/p/original/phmjv93zEwitWLJEOvlXPhtK58o.jpg"
-            alt=""
-            width={200}
-            height={200}
-          />
+          <Image src={backdrop_path} alt="" width={200} height={200} />
         </div>
         <div class="col-span-8">
-          <h2 class="font-bold text-slate-300 text-2xl">
-            Godzilla x Kong: The New Empire
-          </h2>
-          <p class="my-2 text-slate-400 italic">
-            Following their explosive showdown, Godzilla and Kong must reunite
-            against a colossal undiscovered threat hidden within our world,
-            challenging their very existence – and our own.
-          </p>
+          <h2 class="font-bold text-slate-300 text-2xl">{title}</h2>
+          <p class="my-2 text-slate-400 italic">{overview}</p>
           <ul class="text-slate-300 space-y-2 my-8">
             <li>Release Date : 2024-03-27</li>
             <li>Average Vote : 7.5</li>
