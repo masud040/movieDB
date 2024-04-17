658402b02b7e45a710419d9dc94c6fe336c9481a
masud040 <masudinfo040@.com>
2024-04-17 12:18:38 +0600

    implement language switcher

--- app/components/LanguageSwitcher.js
+++ app/components/LanguageSwitcher.js
@@ -1,7 +1,10 @@
 "use client";
 import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
 export default function LanguageSwitcher() {
+  const router = useRouter();
+  const pathname = usePathname();
   const [showMenu, setShowMenu] = useState(false);
   const languages = [
     {
@@ -13,6 +16,18 @@ export default function LanguageSwitcher() {
       language: "Bangla",
     },
   ];
+  const match = languages.find((lang) => pathname.includes(lang.code));
+  const [selectedLang, setSelectedLang] = useState(match ?? languages[0]);
+  const handleChangeLang = (locale, lang) => {
+    setSelectedLang({
+      ...selectedLang,
+      code: locale,
+      language: lang,
+    });
+    setShowMenu(false);
+    router.push(`/${locale}`);
+  };
+
   return (
     <div className="flex items-center gap-4">
       <div className="relative">
@@ -27,7 +42,7 @@ export default function LanguageSwitcher() {
             height={80}
             width={100}
           />
-          Bangla
+          {selectedLang.language}
         </button>
         {showMenu && (
           <ul className="absolute right-0 z-10 w-40 p-2 mt-2 bg-white rounded-md shadow-lg top-full">
@@ -35,6 +50,7 @@ export default function LanguageSwitcher() {
               <li
                 key={entry.code}
                 className="flex items-center gap-2 p-2 text-sm text-black rounded-md cursor-pointer hover:bg-gray-200"
+                onClick={() => handleChangeLang(entry.code, entry.language)}
               >
                 <Image
                   className="max-w-6"
